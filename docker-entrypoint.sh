5a5257f8b6d619ec60df836699c3fefa705d1dae
Vitaly Kovalyshyn <vitaly@kovalyshyn.pp.ua>
2020-01-24 14:26:44 +0200

    rsyslog

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,6 @@ echo "local0.*  /var/log/opensips.log" >>/etc/rsyslog.conf
 if [ "$1" = 'opensips' ]; then
     /opensips/sbin/opensips -E
     rsyslogd -n
-    tail -f /var/log/opensips.log
 fi
 
 exec "$@"
